2bc8f3e00cba07e6e86343af973eb99bda514de2
axzid <storyciks@gmail.com>
2022-11-28 00:52:14 +0700

    blog

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,9 +1,6 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
-  { href: '/contact', title: 'Contact' },
 ]
 
 export default headerNavLinks
