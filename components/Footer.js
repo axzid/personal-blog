79e4e331d190d5268bad969356d669ccd3646223
AleXander Zulfikar <53267831+axzid@users.noreply.github.com>
2023-12-07 10:46:27 +0000

    a

--- components/Footer.js
+++ components/Footer.js
@@ -17,10 +17,7 @@ export default function Footer() {
           <div>{` • `}</div>
           <Link href="/">{siteMetadata.author}</Link>
         </div>
-        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          Build With NextJs - Theme by
-          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog"> timlrx</Link>
-        </div>
+        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">Build With NextJs ♥️</div>
       </div>
     </footer>
   )
